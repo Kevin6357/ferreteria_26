ed4eca1129ae0a11dddd815a22609b9d05c12884
Kevin6357 <1680504@senati.pe>
2026-05-25 01:58:30 -0500

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("page-loader");
+
+    setTimeout(() => {
+        loader.classList.add("hide");
+    }, 1200);
+
+});
