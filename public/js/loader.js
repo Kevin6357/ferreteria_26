60bf304de47472dd85f57454a5e678ba12c1f7d4
Kevin6357 <1680504@senati.pe>
2026-06-03 01:34:15 -0500

    Delete public directory

--- public/js/loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-
-    const loader = document.getElementById("page-loader");
-
-    setTimeout(() => {
-        loader.classList.add("hide");
-    }, 1200);
-
-});
